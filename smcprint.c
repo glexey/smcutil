bb884fcbbaff4b7d6c40d2cd5d24d06401b91ee4
Alexey Gaydyukov <no@email.com>
2015-03-18 14:26:16 -0700

    smcprint to print get_float() from cmd line

--- smcprint.c
+++ smcprint.c
@@ -24,35 +24,40 @@
 #include <stdio.h>
 #include "smc.h"
 
-int main()
+int main(int argc, char* argv[])
 {
-    if (open_smc() != kIOReturnSuccess) {
-        return -1;
-    }
+   if (open_smc() != kIOReturnSuccess) {
+      return -1;
+   }
 
-    printf("AMBIENT_AIR_0: %0.1fC\n", get_tmp(AMBIENT_AIR_0, CELSIUS));
-    printf("AMBIENT_AIR_1: %0.1fC\n", get_tmp(AMBIENT_AIR_1, CELSIUS));
-    printf("CPU_0_DIODE: %0.1fC\n", get_tmp(CPU_0_DIODE, CELSIUS));
-    printf("CPU_0_HEATSINK: %0.1fC\n", get_tmp(CPU_0_HEATSINK, CELSIUS));
-    printf("CPU_0_PROXIMITY: %0.1fC\n", get_tmp(CPU_0_PROXIMITY, CELSIUS));
-    printf("GPU_0_DIODE: %0.1fC\n", get_tmp(GPU_0_DIODE, CELSIUS));
-    printf("GPU_0_HEATSINK: %0.1fC\n", get_tmp(GPU_0_HEATSINK, CELSIUS));
-    printf("GPU_0_PROXIMITY: %0.1fC\n", get_tmp(GPU_0_PROXIMITY, CELSIUS));
-    printf("ENCLOSURE_BASE_0: %0.1fC\n", get_tmp(ENCLOSURE_BASE_0, CELSIUS));
-    printf("ENCLOSURE_BASE_1: %0.1fC\n", get_tmp(ENCLOSURE_BASE_1, CELSIUS));
-    printf("ENCLOSURE_BASE_2: %0.1fC\n", get_tmp(ENCLOSURE_BASE_2, CELSIUS));
-    printf("ENCLOSURE_BASE_3: %0.1fC\n", get_tmp(ENCLOSURE_BASE_3, CELSIUS));
-    printf("HARD_DRIVE_BAY: %0.1fC\n", get_tmp(HARD_DRIVE_BAY, CELSIUS));
-    printf("MEMORY_SLOT_0: %0.1fC\n", get_tmp(MEMORY_SLOT_0, CELSIUS));
-    printf("MEMORY_SLOTS_PROXIMITY: %0.1fC\n", get_tmp(MEMORY_SLOTS_PROXIMITY, CELSIUS));
-    int num_fans = get_num_fans();
-    printf("Num fans: %d\n", num_fans);
-    for (int i=0; i < num_fans; i++)
-	printf("Fan %d: %d RPM\n", i, get_fan_rpm(i));
-    
-    printf("Voltage: %f\n", get_float("VC0C"));
+   //printf("AMBIENT_AIR_0: %0.1fC\n", get_tmp(AMBIENT_AIR_0, CELSIUS));
+   //printf("AMBIENT_AIR_1: %0.1fC\n", get_tmp(AMBIENT_AIR_1, CELSIUS));
+   //printf("CPU_0_DIODE: %0.1fC\n", get_tmp(CPU_0_DIODE, CELSIUS));
+   //printf("CPU_0_HEATSINK: %0.1fC\n", get_tmp(CPU_0_HEATSINK, CELSIUS));
+   //printf("CPU_0_PROXIMITY: %0.1fC\n", get_tmp(CPU_0_PROXIMITY, CELSIUS));
+   //printf("GPU_0_DIODE: %0.1fC\n", get_tmp(GPU_0_DIODE, CELSIUS));
+   //printf("GPU_0_HEATSINK: %0.1fC\n", get_tmp(GPU_0_HEATSINK, CELSIUS));
+   //printf("GPU_0_PROXIMITY: %0.1fC\n", get_tmp(GPU_0_PROXIMITY, CELSIUS));
+   //printf("ENCLOSURE_BASE_0: %0.1fC\n", get_tmp(ENCLOSURE_BASE_0, CELSIUS));
+   //printf("ENCLOSURE_BASE_1: %0.1fC\n", get_tmp(ENCLOSURE_BASE_1, CELSIUS));
+   //printf("ENCLOSURE_BASE_2: %0.1fC\n", get_tmp(ENCLOSURE_BASE_2, CELSIUS));
+   //printf("ENCLOSURE_BASE_3: %0.1fC\n", get_tmp(ENCLOSURE_BASE_3, CELSIUS));
+   //printf("HARD_DRIVE_BAY: %0.1fC\n", get_tmp(HARD_DRIVE_BAY, CELSIUS));
+   //printf("MEMORY_SLOT_0: %0.1fC\n", get_tmp(MEMORY_SLOT_0, CELSIUS));
+   //printf("MEMORY_SLOTS_PROXIMITY: %0.1fC\n", get_tmp(MEMORY_SLOTS_PROXIMITY, CELSIUS));
+   //int num_fans = get_num_fans();
+   //printf("Num fans: %d\n", num_fans);
+   //for (int i=0; i < num_fans; i++)
+   //printf("Fan %d: %d RPM\n", i, get_fan_rpm(i));
+   //printf("Voltage: %f\n", get_float("VC0C"));
 
-    close_smc();
+   for (int i=1; i<argc; i++) {
+      if (i>1) printf(",");
+      printf("%f", get_float(argv[i]));
+   }
+   printf("\n");
 
-    return 0;
+   close_smc();
+
+   return 0;
 }
